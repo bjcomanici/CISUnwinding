f75636506952281d2cd9518abf4d0b46a3c84ef0
bjcomanici <bjcomanici@yahoo.com>
2025-02-14 15:47:35 -0500

    Add files via upload

---
+++ CISLvl2Unwind.ps1
@@ -0,0 +1,40 @@
+﻿#Unwind CIS Hardeing Level 2
+
+
+#Registry Changes
+
+
+#Allow Camera Use
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Camera" -Name "AllowCamera" -Value 1
+
+#Allow Printer Install
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" -Name "AddPrinterDrivers" -Value 0
+
+#Enable Font Providers
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\System" -Name "Enablefontproviders" -Value 1
+
+#Enable Printing over HTTP
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows NT\Printers" -Name "DisableHTTPPrinting" -Value 0
+
+#Enable Location
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\LocationAndSensors" -Name "Disablelocation" -Value 0
+
+#Enable Windows Store
+Set-ItemProperty -Path "HKLM\Software\Policies\Microsoft\WindowsStore" -Name "RemoveWindowsStore" -Value 0
+
+#Enable Access to the Store
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer" -Name "NoUseStoreOpenWith" -Value 0
+
+
+#Service Changes
+
+#Enable Remote Registry
+Set-Service -Name RemoteRegistry -StartupType Automatic
+Start-Service -Name RemoteRegistry
+
+#Enable Spooler
+Set-Service -Name Spooler -StartupType Automatic
+Start-Service -Name Spooler
+
+
+
